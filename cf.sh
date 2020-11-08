a4bcf01a8ab9e54823f5baafd0326061238e9d2b
badafans <990508464@qq.com>
2020-11-08 05:29:42 -0600

    全新版本

--- cf.sh
+++ cf.sh
@@ -12,17 +12,42 @@ do
 		declare -i n
 		declare -i per
 		declare -i count
-		rm -rf icmp temp log.txt anycast.txt
+		rm -rf icmp temp log.txt data.txt anycast.txt
 		mkdir icmp
-		curl https://anycast.freecdn.workers.dev -# -o anycast.txt
+		curl --ipv4 https://service.freecdn.workers.dev -# -o data.txt
+		publicip=$(cat data.txt | grep publicip: | cut -f 2- -d':')
+		domain=$(cat data.txt | grep domain: | cut -f 2- -d':')
+		file=$(cat data.txt | grep file: | cut -f 2- -d':')
+		url=$(cat data.txt | grep url: | cut -f 2- -d':')
+		version=$(cat data.txt | grep version: | cut -f 2- -d':')
+		if [ "$version" != "20201108" ]
+		then
+			echo 发现新版本: $version
+			echo 更新地址: $url
+			echo 更新后才可以使用
+			exit
+		fi
+		n=0
+		count=$(($RANDOM%5))
+		for i in `cat data.txt | sed '1,5d'`
+		do
+			if [ $n -eq $count ]
+			then
+				echo $i>>anycast.txt
+				count+=4
+			else
+				n+=1
+			fi
+		done
+		rm -rf data.txt
 		n=0
 		m=$(cat anycast.txt | wc -l)
-		count=(m+1)/50+1
+		count=m/30+1
 		for i in `cat anycast.txt`
 		do
 			ping -c $count -i 0.2 -n -q $i > icmp/$n.log&
-			per=$n*100/$m
 			n=$[$n+1]
+			per=$n*100/$m
 			while true
 			do
 				p=$(ps -ef | grep ping | grep -v "grep" | wc -l)
@@ -49,15 +74,14 @@ do
 				break
 			fi
 		done
-		cat icmp/*.log | sed -n '3~5p;4~5p' | sed -n '{N;s/\n/\t/p}' | cut -f1 -d'%' | awk '{print $2,$NF}' | sort -k 2 -n | awk '{print $1}' | sed '51,$d' > ip.txt
+		cat icmp/*.log | sed -n '3~5p;4~5p' | sed -n '{N;s/\n/\t/p}' | cut -f 1 -d'%' | awk '{print $2,$NF}' | sort -k 2 -n | awk '{print $1}' | sed '31,$d' > ip.txt
 		rm -rf icmp
-		echo 选取50个丢包率最少的IP地址下载测速
+		echo 选取30个丢包率最少的IP地址下载测速
 		mkdir temp
 		for i in `cat ip.txt`
 		do
 			echo $i 启动测速
-			curl --resolve speed.udpfile.com:443:$i https://speed.udpfile.com/cache.png -o temp/$i -s --connect-timeout 2 --max-time 10&
-			sleep 0.5
+			curl --resolve $domain:443:$i https://$domain/$file -o temp/$i -s --connect-timeout 2 --max-time 10&
 		done
 		echo 等待测速进程结束,筛选出三个优选的IP
 		sleep 15
@@ -72,7 +96,7 @@ do
 			rm -rf ip.txt
 			echo 优选的IP地址为 $first - $second - $third
 			echo 第一次测试 $first
-			curl --resolve speed.udpfile.com:443:$first https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$first https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -106,7 +130,7 @@ do
 			max=$[$max/1024]
 			echo 峰值速度 $max kB/s
 			echo 第二次测试 $first
-			curl --resolve speed.udpfile.com:443:$first https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$first https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -140,7 +164,7 @@ do
 			max=$[$max/1024]
 			echo 峰值速度 $max kB/s
 			echo 第一次测试 $second
-			curl --resolve speed.udpfile.com:443:$second https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$second https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -174,7 +198,7 @@ do
 			max=$[$max/1024]
 			echo 峰值速度 $max kB/s
 			echo 第二次测试 $second
-			curl --resolve speed.udpfile.com:443:$second https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$second https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -208,7 +232,7 @@ do
 			max=$[$max/1024]
 			echo 峰值速度 $max kB/s
 			echo 第一次测试 $third
-			curl --resolve speed.udpfile.com:443:$third https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$third https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -242,7 +266,7 @@ do
 			max=$[$max/1024]
 			echo 峰值速度 $max kB/s
 			echo 第二次测试 $third
-			curl --resolve speed.udpfile.com:443:$third https://speed.udpfile.com/cache.png -o /dev/null --connect-timeout 5 --max-time 15 > log.txt 2>&1
+			curl --resolve $domain:443:$third https://$domain/$file -o /dev/null --connect-timeout 5 --max-time 10 > log.txt 2>&1
 			cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
 			for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
 			do
@@ -284,4 +308,6 @@ done
 	start_seconds=$(date --date="$starttime" +%s)
 	end_seconds=$(date --date="$endtime" +%s)
 	clear
-	echo $anycast 满足 $bandwidth Mbps带宽需求,峰值速度 $max kB/s,总计用时 $((end_seconds-start_seconds)) 秒
+	echo 优选IP $anycast 满足 $bandwidth Mbps带宽需求
+	echo 峰值速度 $max kB/s
+	echo 出口IP $publicip 总计用时 $((end_seconds-start_seconds)) 秒
