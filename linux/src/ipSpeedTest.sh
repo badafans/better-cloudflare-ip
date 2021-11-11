df7838c8a62bd40784c6b5dff96a9bbd45b49599
An <676412+Ansen@users.noreply.github.com>
2021-11-11 14:57:38 +0800

    Create ipSpeedTest.sh

---
+++ linux/src/ipSpeedTest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. /etc/profile
+
+function main(){
+    local test_ip="$1"
+    local update_url='update.udpfile.com'
+    local tmp_file="/tmp/ip_test.tmp"
+
+    if [[  -z "$test_ip" ]]; then
+        echo "Usage: $0 <ip>"
+        exit 1
+    elif [[ "$test_ip" != "${test_ip#*[0-9].[0-9]}" ]]; then
+        :
+    elif [[ "$test_ip" != "${test_ip#*:[0-9a-fA-F]}" ]]; then
+        :
+    else
+        echo "Unrecognized IP format '$1'"
+        exit 1
+    fi
+
+    curl -s -q --resolve ${update_url}:443:${test_ip} https://${update_url} -o ${tmp_file}
+
+    [[ $? -ne 0 ]] && echo "获取测速信息失败" && exit 1
+
+    local speedtest_url=$(awk -F= '/^domain/ {print $2}' ${tmp_file})
+    local speedtest_file=$(awk -F= '/^file/ {print $2}' ${tmp_file})
+    
+
+    curl --resolve ${speedtest_url}:443:${test_ip} https://${speedtest_url}/${speedtest_file} -o /dev//null
+
+}
+
+main "$@"
